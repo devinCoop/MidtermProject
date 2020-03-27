a2eb246eec6f15e720626195357bd003cbe4d12b
sethschn <sethschn301@gmail.com>
2020-03-27 13:58:50 -0600

    Fixed application props and persistance info, got a working user entity tested

--- JPAQuarAngel/src/test/java/com/skilldistillery/quarangel/entities/UserTest.java
+++ JPAQuarAngel/src/test/java/com/skilldistillery/quarangel/entities/UserTest.java
@@ -1,34 +1,51 @@
 package com.skilldistillery.quarangel.entities;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
 
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class UserTest {
+	private EntityManager em;
+	static EntityManagerFactory emf;
+	private User user;
 
 	@BeforeAll
 	static void setUpBeforeClass() throws Exception {
+		emf = Persistence.createEntityManagerFactory("QuarAngelPU");
 	}
 
 	@AfterAll
 	static void tearDownAfterClass() throws Exception {
+		emf.close();
 	}
 
 	@BeforeEach
 	void setUp() throws Exception {
+		em = emf.createEntityManager();
+		user = em.find(User.class, 1);
 	}
 
 	@AfterEach
 	void tearDown() throws Exception {
+		em.close();
+		user = null;
 	}
 
 	@Test
+	@DisplayName("user test")
 	void test() {
-		fail("Not yet implemented");
+		assertEquals("seths", user.getUsername());
+		//fail("Not yet implemented");
 	}
 
 }
