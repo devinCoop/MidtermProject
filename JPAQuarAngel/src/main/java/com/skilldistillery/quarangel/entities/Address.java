be4ea4f3450e0a93c2553c0ece6be8d9e36bf20c
Janice Ellsworth <janice.ellsworth2@gmail.com>
2020-03-28 12:06:02 -0600

    Commiting Address Entity and PASSED AddressTest

---
+++ JPAQuarAngel/src/main/java/com/skilldistillery/quarangel/entities/Address.java
@@ -0,0 +1,97 @@
+package com.skilldistillery.quarangel.entities;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Address {
+
+	// FIELDS
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	private String street;
+	private String city;
+	private String state;
+	@Column(name = "zip_code")
+	private int zipCode;
+
+	// METHODS BEGIN:
+
+	public Address() {
+
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getStreet() {
+		return street;
+	}
+
+	public void setStreet(String street) {
+		this.street = street;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public String getState() {
+		return state;
+	}
+
+	public void setState(String state) {
+		this.state = state;
+	}
+
+	public int getZipCode() {
+		return zipCode;
+	}
+
+	public void setZipCode(int zipCode) {
+		this.zipCode = zipCode;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + id;
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Address other = (Address) obj;
+		if (id != other.id)
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "Address [id=" + id + ", street=" + street + ", city=" + city + ", state=" + state + ", zipCode="
+				+ zipCode + "]";
+	}
+	
+	
+}
---
+++ JPAQuarAngel/src/test/java/com/skilldistillery/quarangel/entities/AddressTest.java
@@ -0,0 +1,53 @@
+package com.skilldistillery.quarangel.entities;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class AddressTest {
+	private EntityManager em;
+	static EntityManagerFactory emf;
+	private Address address;
+
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+		emf = Persistence.createEntityManagerFactory("QuarAngelPU");
+	}
+
+	@AfterAll
+	static void tearDownAfterClass() throws Exception {
+		emf.close();
+	}
+
+	@BeforeEach
+	void setUp() throws Exception {
+		em = emf.createEntityManager();
+		address = em.find(Address.class, 2);
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+		em.close();
+		address = null;
+	}
+
+	@Test
+	@DisplayName("address entity test")
+	void test() {
+		assertEquals("1002 burnwood drive", address.getStreet());
+		assertEquals("denver", address.getCity());
+		assertEquals("colorado", address.getState());
+		assertEquals(55555, address.getZipCode());
+	}
+
+}
