e5dd8203a241e5a800a106823f3ca7b0f9550cbc
Janice Ellsworth <janice.ellsworth2@gmail.com>
2020-04-01 10:23:07 -0600

    Fixed camel casing on all controllers and JSPs

--- QuarAngel/src/main/java/com/skilldistillery/quarangel/controllers/LoginController.java
+++ QuarAngel/src/main/java/com/skilldistillery/quarangel/controllers/LoginController.java
@@ -1,5 +1,7 @@
 package com.skilldistillery.quarangel.controllers;
 
+import java.time.LocalDateTime;
+
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,7 +10,9 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import com.skilldistillery.quarangel.data.CategoryDAO;
 import com.skilldistillery.quarangel.data.UserDAO;
+import com.skilldistillery.quarangel.entities.Task;
 import com.skilldistillery.quarangel.entities.User;
 
 @Controller
@@ -16,6 +20,9 @@ public class LoginController {
 
 	@Autowired
 	private UserDAO dao;
+	
+	@Autowired
+	private CategoryDAO catDAO;
 
 	@RequestMapping(path = "login.do", method = RequestMethod.POST)
 	public String login(User user, Model model, HttpSession session) {
@@ -26,7 +33,25 @@ public class LoginController {
 		} else {
 			session.setAttribute("loggedInUser", userObj);
 		}
-		return "dashboard";
+		return "loginLandingPage";
+
+	}
+
+	@RequestMapping(path = "loggingIn.do", method = RequestMethod.POST)
+	public String loggingIn(User user, Model model, HttpSession session) {
+		System.out.println(user.getUsername() + "      " + user.getPassword());
+		User userObj = dao.findByUsernameAndPassword(user.getUsername(), user.getPassword());
+		if (userObj == null) {
+			return "index";
+		} else {
+			session.setAttribute("loggedInUser", userObj);
+		}
+		Task task = new Task();
+		model.addAttribute("categories", catDAO.findAll());
+		task.setDateDeadline(LocalDateTime.now());
+		task.setDateCreated(LocalDateTime.now());
+		model.addAttribute("task", task);
+		return "loginLandingPage";
 
 	}
 
